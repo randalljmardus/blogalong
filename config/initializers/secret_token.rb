40b6461bb82efc3a53599802726b8d2286cf98c8
zkirchin <zacharykirchin@gmail.com>
2014-05-22 09:42:25 -0400

    All files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Blogalong::Application.config.secret_key_base = '6eee711de1fd4b45f828a41b6ad3b99b5750f3971a7494f45d650f0b0b6055915da98a4f7ba26a3247274480e9e7284422a8c5d41ac7cb58cf657479a1f9cea4'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Blogalong::Application.config.session_store :cookie_store, key: '_Blogalong_session'
